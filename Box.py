fd62f4d31fc8f268c80bc0ae8a9039e2d9595d10
KnutSab <42932604+KnutSab@users.noreply.github.com>
2018-09-05 17:45:26 +0200

    Update Box.py

--- Box.py
+++ Box.py
@@ -10,4 +10,4 @@ posisjon = Box.PositionVelocityUpdate()
 #print(posisjon)
 """
 Box = cf.integral(0,1e-6,3.3474472*(10**(-27)),3e3,0,1000,int(1e5),(1e-13))
-
+posisjon = Box.PositionVelocityUpdate()
