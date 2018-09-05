e75551b8021bcae2b1d1ad36f80dfe4eef0a8132
KnutSab <42932604+KnutSab@users.noreply.github.com>
2018-09-05 16:34:38 +0200

    Create Box.py
    Fil som brukes til å teste rakettmotor

---
+++ Box.py
@@ -0,0 +1,13 @@
+import class_file as cf
+import random as random
+import numpy as np
+import matplotlib.pyplot as plt
+"""
+random.seed(69)
+
+Box = cf.integral(0,1e-6,3.3474472*(10**(-27)),3e3,0,1000,int(1e5),(1e-13))
+posisjon = Box.PositionVelocityUpdate()
+#print(posisjon)
+"""
+Box = cf.integral(0,1e-6,3.3474472*(10**(-27)),3e3,0,1000,int(1e5),(1e-13))
+
