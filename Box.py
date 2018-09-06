dd74e547caa9e01fdcbbd4a7fde465a55263f41b
KnutSab <42932604+KnutSab@users.noreply.github.com>
2018-09-06 15:09:30 +0200

    Update Box.py
    Still a testing file

--- Box.py
+++ Box.py
@@ -1,13 +1,17 @@
 import class_file as cf
-import random as random
 import numpy as np
 import matplotlib.pyplot as plt
-"""
-random.seed(69)
+import numba
 
+np.random.seed(69)
+"""
 Box = cf.integral(0,1e-6,3.3474472*(10**(-27)),3e3,0,1000,int(1e5),(1e-13))
 posisjon = Box.PositionVelocityUpdate()
 #print(posisjon)
 """
-Box = cf.integral(0,1e-6,3.3474472*(10**(-27)),3e3,0,1000,int(1e5),(1e-13))
-posisjon = Box.PositionVelocityUpdate()
+Box = cf.integral(0,1e-6,3.3474472*(10**(-27)),3e3,0,int(1e5),int(1e3),(1e-9))
+#posisjon = Box.PositionVelocityUpdate()
+FA = Box.BoxForceCounter()
+#print("Total Force: {:g}, Number of Chambers: {:g}".format(FA[0], FA[1]))
+#Ost = Box.FuelConsup()
+print(FA)
