e8c441d45a926318b0b66d1b98c4f5b163498f92
KnutSab <42932604+KnutSab@users.noreply.github.com>
2018-09-12 19:03:56 +0200

    Update Orbits.py

--- Orbits.py
+++ Orbits.py
@@ -1,10 +1,7 @@
 from AST2000SolarSystem import AST2000SolarSystem
 import numpy as np
 import matplotlib.pyplot as plt
-def planet_orbit(e,a,theta,sigma):
-    f = theta - sigma
-    r = a*(1-e**2)/(1+e*np.cos(f))
-    return r
+
 #planet_orbit(e,a,)
 
 
@@ -29,41 +26,39 @@ vx0 = attribute.vx0 #x-komponent til farten til planetene
 vy0 = attribute.vy0 #y-komponent til farten til planetene
 x0 = attribute.x0 #x-koordinat for utgangsposisjonen
 y0 = attribute.y0 #y-koordinat for utgangsposisjonen
-print(period)
 
-#print(vx0[0], vy0)
-"""
-#print(x0)
-plt.plot(0,0,'bo')
-plt.plot(x0[0],y0[0], 'ro')
-plt.plot(x0[1],y0[1], 'go')
-plt.plot(x0[2],y0[2], 'co')
-plt.plot(x0[3],y0[3], 'mo')
-plt.plot(x0[4],y0[4], 'ko')
-plt.plot(x0[5],y0[5], 'ro')
-plt.plot(x0[6],y0[6], 'yo')
-plt.show()
-"""
-def integrator(x0,v0,T,dt):
-    G = 6.67*10e-11
-    def R(t):
-        F = -((G*star_mass)/(np.sqrt(x0**2 + y0**2)**3))*(x0+y0)
-        return F
+
+def integrator(x0,y0,vx0,vy0,T,dt):
     N = int(T/dt)
-    t = np.zeros(N)
-    x = x0
-    v = v0
-    a_i = R(t)
+    G = 4*np.pi**2
+    ax = np.zeros(N+1)
+    ay = np.zeros(N+1)
+    x = np.zeros(N+1)
+    y = np.zeros(N+1)
+    x[0] = x0
+    y[0] = y0
+    vx = np.zeros(N+1)
+    vy = np.zeros(N+1)
+    vx[0] = vx0
+    vy[0] = vy0
     for i in range(N):
-        a_iplus1 = R(t)
-    for i in range(N):
-        t += dt
-        x += v*dt + 0.5* a_i*dt**2
-        a_iplus1 = R(t)
-        v += 0.5*(a_i + a_iplus1)*dt
-        a_i = a_iplus1
-    return x
-pos = integrator(x0,y0,10,0.001)
-print(pos)
-plt.plot(pos)
-plt.show()
+        
+        ax[i] = (-(G*star_mass)/(x[i]**2 + y[i]**2) * (x[i]/(np.sqrt(x[i]**2 + y[i]**2))))
+        ay[i] = (-(G*star_mass)/(x[i]**2 + y[i]**2) * (y[i]/(np.sqrt(x[i]**2 + y[i]**2))))
+        vx[i+1] = vx[i] + ax[i]*dt
+        vy[i+1] = vy[i] + ay[i]*dt
+        x[i+1] = x[i] + vx[i+1]*dt
+        y[i+1] = y[i] + vy[i+1]*dt
+
+    return(x,y)
+
+#orbit = integrator(x0[0],y0[0],vx0[0],vy0[0],20,0.00001)
+x1 = integrator(x0[0],y0[0],vx0[0],vy0[0],20,0.00001)[0],integrator(x0[0],y0[0],vx0[0],vy0[0],20,0.00001)[1])
+x2 = integrator(x0[1],y0[1],vx0[1],vy0[1],20,0.00001)[0],integrator(x0[1],y0[1],vx0[1],vy0[1],20,0.00001)[1])
+x3 = integrator(x0[2],y0[2],vx0[2],vy0[2],20,0.00001)[0],integrator(x0[2],y0[2],vx0[2],vy0[2],20,0.00001)[1])
+x4 = integrator(x0[3],y0[3],vx0[3],vy0[3],20,0.00001)[0],integrator(x0[3],y0[3],vx0[3],vy0[3],20,0.00001)[1])
+x5 = integrator(x0[4],y0[4],vx0[4],vy0[4],20,0.00001)[0],integrator(x0[4],y0[4],vx0[4],vy0[4],20,0.00001)[1])
+x6 = integrator(x0[5],y0[5],vx0[5],vy0[5],20,0.00001)[0],integrator(x0[5],y0[5],vx0[5],vy0[5],20,0.00001)[1])
+x7 = integrator(x0[6],y0[6],vx0[6],vy0[6],20,0.00001)[0],integrator(x0[6],y0[6],vx0[6],vy0[6],20,0.00001)[1])
+#plt.axis("equal")
+#plt.plot(orbit[0],orbit[1])
