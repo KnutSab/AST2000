b5fce0af9f75de97f5d0f9f15846cb6147ed774c
KnutSab <42932604+KnutSab@users.noreply.github.com>
2018-09-13 19:00:29 +0200

    Update Orbits.py

--- Orbits.py
+++ Orbits.py
@@ -1,14 +1,14 @@
 from AST2000SolarSystem import AST2000SolarSystem
 import numpy as np
 import matplotlib.pyplot as plt
-
+import os
 #planet_orbit(e,a,)
 
 
 
 #planet orbit =
 attribute = AST2000SolarSystem(40689)
-a = attribute.a #Planetenes halvakser
+a_p = attribute.a #Planetenes halvakser
 area_lander = attribute.area_lander #overflateareal lander
 area_sat = attribute.area_sat #overflateareal satelitt
 e = attribute.e #eksentrisitet planeter
@@ -26,67 +26,113 @@ vx0 = attribute.vx0 #x-komponent til farten til planetene
 vy0 = attribute.vy0 #y-komponent til farten til planetene
 x0 = attribute.x0 #x-koordinat for utgangsposisjonen
 y0 = attribute.y0 #y-koordinat for utgangsposisjonen
+#print(star_mass)
 
-"""
-
-def integrator(x0,y0,vx0,vy0,T,dt):
-    N = int(T/dt)
-    G = 4*np.pi**2
-    ax = np.zeros(N+1)
-    ay = np.zeros(N+1)
-    x = np.zeros(N+1)
-    y = np.zeros(N+1)
-    x[0] = x0
-    y[0] = y0
-    vx = np.zeros(N+1)
-    vy = np.zeros(N+1)
-    vx[0] = vx0
-    vy[0] = vy0
-    pos = np.array((x0,y0))
-    for i in range(N):
-        new_pos[i] =
-        ax[i] = (-(G*star_mass)/(x[i]**2 + y[i]**2) * (x[i]/(np.sqrt(x[i]**2 + y[i]**2))))
-        ay[i] = (-(G*star_mass)/(x[i]**2 + y[i]**2) * (y[i]/(np.sqrt(x[i]**2 + y[i]**2))))
-        vx[i+1] = vx[i] + ax[i]*dt
-        vy[i+1] = vy[i] + ay[i]*dt
-        x[i+1] = x[i] + vx[i+1]*dt
-        y[i+1] = y[i] + vy[i+1]*dt
-
-    return(x,y)
-
-#orbit = integrator(x0[0],y0[0],vx0[0],vy0[0],20,0.00001)
-x1 = integrator(x0[0],y0[0],vx0[0],vy0[0],20,0.00001)[0],integrator(x0[0],y0[0],vx0[0],vy0[0],20,0.00001)[1])
-x2 = integrator(x0[1],y0[1],vx0[1],vy0[1],20,0.00001)[0],integrator(x0[1],y0[1],vx0[1],vy0[1],20,0.00001)[1])
-x3 = integrator(x0[2],y0[2],vx0[2],vy0[2],20,0.00001)[0],integrator(x0[2],y0[2],vx0[2],vy0[2],20,0.00001)[1])
-x4 = integrator(x0[3],y0[3],vx0[3],vy0[3],20,0.00001)[0],integrator(x0[3],y0[3],vx0[3],vy0[3],20,0.00001)[1])
-x5 = integrator(x0[4],y0[4],vx0[4],vy0[4],20,0.00001)[0],integrator(x0[4],y0[4],vx0[4],vy0[4],20,0.00001)[1])
-x6 = integrator(x0[5],y0[5],vx0[5],vy0[5],20,0.00001)[0],integrator(x0[5],y0[5],vx0[5],vy0[5],20,0.00001)[1])
-x7 = integrator(x0[6],y0[6],vx0[6],vy0[6],20,0.00001)[0],integrator(x0[6],y0[6],vx0[6],vy0[6],20,0.00001)[1])
-#plt.axis("equal")
-#plt.plot(orbit[0],orbit[1])
-"""
 pos0 = np.array((x0,y0))
 vel0 = np.array((vx0,vy0))
-
+print(a_p[2])
 N_planets = number_of_planets
-N_time = 150000
+#N_time = 200000
+
+
+#T = antall år på bane * Antall år ønslket
+#N = Tidssteg * T
+#N_time = T*N
+P = np.sqrt(a_p[0]**3/(star_mass+mass[0]))
+print(P)
+T = P*21
+N = T*10000
+N_time = int(T*N)
+dt = T/10000
+
 pos = np.zeros((N_time, 2, N_planets))
 vel = np.zeros((N_time, 2, N_planets))
 a = np.zeros((N_time,2,N_planets))
 G = 4*np.pi**2
+time = np.zeros(N_time)
 pos[0] = pos0
 vel[0] = vel0
-dt = 0.0001
+a[0] = (-(G*star_mass)/(pos[0,0]**2 + pos[0,1]**2) * (pos[0]/(np.sqrt(pos[0,0]**2 + pos[0,1]**2))))
+p = 1
 for i in range(N_time-1):
-    temp = (-(G*star_mass)/(pos[i,0]**2 + pos[i,1]**2) * (pos[i]/(np.sqrt(pos[i,0]**2 + pos[i,1]**2))))
-    print(temp.shape)
-    print(a[i].shape)
-    a[i] = temp
+    pos[i+1] = pos[i] + vel[i]*dt + 0.5*a[i]*(dt**2)
+    a[i+1] = (-(G*star_mass)/(pos[i+1,0]**2 + pos[i+1,1]**2) * (pos[i+1]/(np.sqrt(pos[i+1,0]**2 + pos[i+1,1]**2))))
+    vel[i+1] = vel[i] + 0.5*(a[i]+a[i+1])*dt
+    if p == 1:
+        if i/N_time > 0.1:
+            print("10%")
+            p = 2
 
+    if p == 2:
+        if i/N_time > 0.2:
+            print("20%")
+            p = 3
+    if p == 3:
+        if i/N_time > 0.3:
+            print("30%")
+            p = 4
+    if p == 4:
+        if i/N_time > 0.4:
+            print("40%")
+            p = 5
+    if p == 5:
+        if i/N_time > 0.5:
+            print("50%")
+            p = 6
+    if p == 6:
+        if i/N_time > 0.6:
+            print("60%")
+            p = 7
+    if p == 7:
+        if i/N_time > 0.7:
+            print("70%")
+            p = 8
+    if p == 8:
+        if i/N_time > 0.8:
+            print("80%")
+            p = 9
+    if p == 9:
+        if i/N_time > 0.9:
+            print("90%")
+            p = 10
+    if p == 10:
+        if i/N_time > 1:
+            print("FINISHED")
+            p = 11
+
+    #time[i+1] = time[i] + dt
+pos2 = np.zeros((2, N_planets,N_time))
+for i in range(N_time-1):
+    for j in range(N_planets):
+
+        pos2[0,j,i] = pos[i,0,j]
+        pos2[1,j,i] = pos[i,1,j]
+
+#print(pos2)
+#print(pos)
+
+print(len(pos))
+print(T*N)
+#print(T)
+#print(N)
+#print(t)
+#print(t)
+
+"""
+    a[i] = (-(G*star_mass)/(pos[i,0]**2 + pos[i,1]**2) * (pos[i]/(np.sqrt(pos[i,0]**2 + pos[i,1]**2))))
     vel[i+1] += (vel[i] + a[i]*dt)
     pos[i+1] += (pos[i] + vel[i+1]*dt)
-print(pos)
+"""
+#print(o[0])
+#T = 2*np.pi*np.sqrt((o[0]**3)/((4*np.pi**2)*(mass[0] + star_mass)))
+#print(T)
+#dt*N_time
+#print(pos[-1])
+
+
+#print(20*P)
+attribute.check_planet_positions(pos2,(T),(N),writeFile=True)
+#os.system('say "Program fullfoert"')
 plt.axis("equal")
-plt.plot(pos[:,0],pos[:,1])
+plt.plot(pos[:,1],pos[:,0])
 plt.show()
-print(mass)
